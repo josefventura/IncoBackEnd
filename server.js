85fef7df4489da34bc6be509512ce9ca21f1e987
josef <jventura@dm5i.com>
2022-01-08 13:26:50 -0300

    setting server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+let port = 9000;
 
-
+app.use(bodyParser.json());
 app.get('/*',(req, res)=>{
-    res.send('hi');
 })
-app.listen(9000, (err)=>{
+app.listen(port, (err)=>{
     if(err)return console.log(err);
-    console.log('is on');
+    console.log(`is live on port : ${port}`);
 })
